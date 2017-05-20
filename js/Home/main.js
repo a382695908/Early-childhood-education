42c42fc8103b555274ca7b8b96200f2d9c5045e3
sun8 <1050211033@qq.com>
2017-05-20 19:00:45 +0800

    5.20

---
+++ js/Home/main.js
@@ -0,0 +1,52 @@
+let num = 0;
+const $pic = $('#pic');
+const $span = $('.circular').find('span');
+let onOff = true;
+let timer = null;
+let arr = ['images/Home/2.png', 'images/Home/3.png', 'images/Home/4.png', 'images/Home/5.png'];
+$span[0].className = 'active';
+$('#next').click(function() {
+    num++;
+    tab();
+});
+$('#prev').click(function() {
+    num--;
+    tab();
+});
+timer = setInterval(function() {
+    num++;
+    tab();
+}, 3000);
+
+$('.banner').hover(function() {
+    clearInterval(timer);
+}, function() {
+    timer = setInterval(function() {
+        num++;
+        tab();
+    }, 3000);
+});
+
+
+function tab() {
+    if (!onOff) return;
+    onOff = false;
+    if (num > arr.length - 1) {
+        num = 0;
+    } else if (num < 0) {
+        num = arr.length - 1;
+    }
+    $span.eq(num).addClass('active').siblings().removeClass('active');
+
+    $pic.animate({
+        opacity: 0.2
+    }, function() {
+        $pic.attr('src', arr[num]);
+
+        $pic.animate({
+            opacity: 1
+        }, function() {
+            onOff = true;
+        });
+    });
+}
